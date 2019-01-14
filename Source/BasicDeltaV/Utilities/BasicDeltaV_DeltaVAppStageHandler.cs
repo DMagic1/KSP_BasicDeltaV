62c3bf5ed1fcaad06bdbc25842b5bde7b10ac87d
DMagic1 <david.grandy@gmail.com>
2019-01-14 09:54:46 -0500

    Delta V App listener scripts

---
+++ Source/BasicDeltaV/Utilities/BasicDeltaV_DeltaVAppStageHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace BasicDeltaV
+{
+    public class BasicDeltaV_DeltaVAppStageHandler : MonoBehaviour
+    {
+        public class DeltaVAppStageStart : UnityEvent<DeltaVAppStageInfo> { }
+        public class DeltaVAppStageDestroy : UnityEvent<DeltaVAppStageInfo> { }
+
+        public static DeltaVAppStageStart OnDVAppStageStart = new DeltaVAppStageStart();
+        public static DeltaVAppStageDestroy OnDVAppStageDestroy = new DeltaVAppStageDestroy();
+
+        private DeltaVAppStageInfo dvApp;
+
+        private void Start()
+        {
+            dvApp = GetComponent<DeltaVAppStageInfo>();
+
+            if (dvApp != null)
+                OnDVAppStageStart.Invoke(dvApp);
+        }
+
+        private void OnDestroy()
+        {
+            if (dvApp != null)
+                OnDVAppStageDestroy.Invoke(dvApp);
+        }
+    }
+}
---
+++ Source/BasicDeltaV/Utilities/BasicDeltaV_DeltaVSituationHandler.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace BasicDeltaV
+{
+    public class BasicDeltaV_DeltaVSituationHandler : MonoBehaviour
+    {
+        public class DeltaVAppSituationAwake: UnityEvent<DeltaVAppSituation> { }
+        public class DeltaVAppSituationStart : UnityEvent<DeltaVAppSituation> { }
+        public class DeltaVAppSituationDestroy : UnityEvent<DeltaVAppSituation> { }
+
+        public static DeltaVAppSituationAwake OnDVAppSituationAwake = new DeltaVAppSituationAwake();
+        public static DeltaVAppSituationStart OnDVAppSituationStart = new DeltaVAppSituationStart();
+        public static DeltaVAppSituationDestroy OnDVAppSituationDestroy = new DeltaVAppSituationDestroy();
+
+        private DeltaVAppSituation dvApp;
+
+        private void Awake()
+        {
+            dvApp = GetComponent<DeltaVAppSituation>();
+
+            if (dvApp != null)
+                OnDVAppSituationAwake.Invoke(dvApp);
+        }
+
+        private void Start()
+        {
+            if (dvApp != null)
+                OnDVAppSituationStart.Invoke(dvApp);
+        }
+
+        private void OnDestroy()
+        {
+            if (dvApp != null)
+                OnDVAppSituationDestroy.Invoke(dvApp);
+        }
+    }
+}
